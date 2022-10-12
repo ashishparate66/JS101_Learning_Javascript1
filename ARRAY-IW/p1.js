584752607d7dc5e147edc5ba97c74c4980d98caf
unknown <ashishparate66@gmail.com>
2022-10-12 13:25:53 +0530

    added array code

---
+++ ARRAY-IW/p1.js
@@ -0,0 +1,8 @@
+// Problem 1 : Given an array print the position (starting with 1) and the element in a single line.
+
+
+let arr = ["Ashish","Pratik","Dipesh","Harshal","Santosh"]
+for(let i=0; i<=arr.length-1;i++){
+console.log((i+1)+". "+arr[i])
+}
+
